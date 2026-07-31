df212eade61a51014e965886abf7ae63b49ad570
Lusail Technology <info@lusailtechnology.com>
2026-07-31 19:11:50 +0530

    Add Nitro Vercel preset to fix 404 on page refresh.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    tailwindcss(),
     tanstackStart({
-      server: {
-        entry: "./src/server.ts",
-      },
+      srcDirectory: "src",
     }),
     viteReact(),
-    tailwindcss(),
+    nitro({
+      preset: "vercel",
+    }),
   ],
 });
